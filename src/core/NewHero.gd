fb60f3d205c6168e4ffccbd2644e2e4d63d10065
ashriot <abray@me.com>
2021-05-10 22:16:02 -0400

    new hero updates

--- src/core/NewHero.gd
+++ src/core/NewHero.gd
@@ -6,3 +6,11 @@ func init(game:Game) -> void:
 
 func setup() -> void:
 	pass
+
+
+func _on_PrevBtn_pressed():
+	pass # Replace with function body.
+
+
+func _on_NextBtn_pressed():
+	pass # Replace with function body.
